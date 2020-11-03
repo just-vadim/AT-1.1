9ee29a72d83706aa75747b7a71a45c92a05a21f3
Vadim <vadim.kurn@gmail.com>
2020-11-03 20:42:25 +0300

    Tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturn500() {
+        int actual = service.remain(500);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn1() {
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn999() {
+        int actual = service.remain(1);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn1000If1000() {
+        int actual = service.remain(1000);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn1000If0() {
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+}

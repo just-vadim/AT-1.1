95e2e7cf7e5bddd4967cccab44a0fdcccc78cebe
Vadim <vadim.kurn@gmail.com>
2020-11-06 19:13:37 +0300

    build.gradle & CashbackHackServiceTest.java edied

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,8 @@
 package ru.netology.service;
 
+import org.testng.annotations.Test;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -30,9 +29,9 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldReturn1000If1000() {
+    public void shouldReturn0If1000() {
         int actual = service.remain(1000);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(actual, expected);
     }
 

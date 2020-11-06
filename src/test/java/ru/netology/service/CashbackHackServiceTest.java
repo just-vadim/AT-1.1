e259323fedbf278d06561e8563f46a60bb8744db
Vadim <vadim.kurn@gmail.com>
2020-11-06 19:50:15 +0300

    build.gradle & CashbackHackServiceTest.java edied

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -11,34 +11,34 @@ public class CashbackHackServiceTest {
     public void shouldReturn500() {
         int actual = service.remain(500);
         int expected = 500;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturn1() {
         int actual = service.remain(999);
         int expected = 1;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturn999() {
         int actual = service.remain(1);
         int expected = 999;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldReturn1000If1000() {
+    public void shouldReturn0If1000() {
         int actual = service.remain(1000);
-        int expected = 1000;
-        assertEquals(actual, expected);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturn1000If0() {
         int actual = service.remain(0);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
